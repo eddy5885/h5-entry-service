bce36bf8c022bbf6f65309b94b3ce93c4897929c
zhangsixue <zhangsixue@xiaomi.com>
2021-02-26 16:17:54 +0800

    first commit

---
+++ app/extend/request.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const parse = require('parseurl');
+
+module.exports = {
+  // 修复 xxx/ 时 404
+  get path() {
+    let pathname = parse(this).pathname;
+    if (pathname.slice(-1) === '/') {
+      pathname += 'index.html';
+    }
+    return pathname;
+  },
+};
